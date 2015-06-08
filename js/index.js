c4fb67b6d3ec27914fb32929eda0eaad3f20ebae
CreatorLanslot <creatorlanslot@gmail.com>
2015-06-08 20:40:49 +0800

    demo

--- js/index.js
+++ js/index.js
@@ -3,3 +3,17 @@
  * @version           1.0.0
  * @last-time         2015.6.7
  */
+
+$(document).ready(function() {
+	iframeInitialize(iframe);
+});
+
+
+// iframe 初始化
+// @para: [iframName]
+function iframeInitialize (iframeName) {
+	var iframe,
+		iframeHeight = (document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight) - 50 + 'px';
+	iframeName ? iframe = $("iframe[name=iframeName]") : iframe = $("iframe");
+	iframe.css('height', iframeHeight);
+}
